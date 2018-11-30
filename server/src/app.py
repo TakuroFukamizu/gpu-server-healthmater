75d4269d2f11a6949004b8181677160c701cf233
Takuro Fukamizu <takuro.f.1987@gmail.com>
2018-11-30 16:05:52 +0900

    fix port lifecycle

--- server/src/app.py
+++ server/src/app.py
@@ -15,7 +15,7 @@ SERIAL_PORT = env.str("SERIAL_PORT", default=8080)
 
 # -----------
 
-def task():
+def task(ser):
     records = []
     try:
         for record in get_smi_record():
@@ -25,18 +25,23 @@ def task():
         print(traceback.format_exc())
         return
     gpu_value = records[0].gpu_utilization
-    print(time.time(), gpu_value)
 
-    with serial.Serial(SERIAL_PORT, 9600, timeout=1) as ser:
-        value = int(gpu_value)
-        value = value.to_bytes(1, 'little')
-        ser.write(value)
-    ser.close()
+    # with serial.Serial(SERIAL_PORT, 9600, timeout=1) as ser:
+    #     value = int(gpu_value)
+    #     value = value.to_bytes(1, 'little')
+    #     ser.write(value)
+    # ser.close()
+    value = int(gpu_value)
+    value = value.to_bytes(1, 'little')
+    ser.write(value)
+    print(time.time(), gpu_value, ser.name, value)
 
 
 if __name__ == "__main__":
-    while True:
-        task()
-        time.sleep(1)
+    with serial.Serial(SERIAL_PORT, 9600, timeout=1) as ser:
+        while True:
+            task(ser)
+            time.sleep(1)
+    ser.close()
 
 
